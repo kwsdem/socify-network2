c8e0e74e3d39ebfaea81bd5165a202882a4fe78b
Giannis Dem <giannidis50@hotmail.com>
2016-01-17 18:57:53 +0200

    deleted auto-html code

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -23,11 +23,5 @@ class Comment < ActiveRecord::Base
   validates_presence_of :commentable
   validates_presence_of :user
 
-  auto_html_for :comment do
-    html_escape
-    image
-    youtube(width: 400, height: 250, autoplay: true)
-    link target: '_blank', rel: 'nofollow'
-    simple_format
-  end
+
 end
