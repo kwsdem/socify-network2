618c4a7779ecd0a3d33e8cf8de922cdc46cdcb95
Giannis Dem <giannidis50@hotmail.com>
2016-01-20 15:03:44 +0200

    try to solve id heroku problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :about, :avatar, :cover,
-                                 :sex, :dob, :location, :url, :birthday)
+                                 :sex, :dob, :location, :id, :url, :birthday)
   end
 
   def check_ownership
